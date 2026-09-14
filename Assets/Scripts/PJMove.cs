0cacfb521f4fca9a2b922eb81d28ae311d05fd2f
CONCEPT <arthurbranquinho250@gmail.com>
2026-09-14 08:42:31 -0300

    Updating

--- Assets/Scripts/PJMove.cs
+++ Assets/Scripts/PJMove.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PJMove : MonoBehaviour{
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     float xVelocity = 5.0f;
+    float jumpForce = 10.0f;
     public GameObject cam;
     public Rigidbody2D rb;
     void Start()
@@ -16,5 +17,10 @@ public class PJMove : MonoBehaviour{
         // Movimentação
         float mX = Input.GetAxisRaw("Horizontal");
         rb.linearVelocityX = mX * xVelocity;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocityY = jumpForce;
+        }
     }
 }
